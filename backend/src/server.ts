6278205a7835a7ba205a88a40f1119e40f11de18
shyam9493 <shyamnandapuneedi123@gmail.com>
2025-10-24 00:57:02 +0530

    fix: add TypeScript types to request and response in server route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req:Request, res:Response) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Server running on port ${port}!`))
